be678e161a76206c070daa5c0911a08d6771974b
Clément Picot <clement.picot@gmail.com>
2021-09-16 14:33:23 +0200

    Initial Commit

---
+++ favqs/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  favqs
+//
+//  Created by Clement Picot on 16/09/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
